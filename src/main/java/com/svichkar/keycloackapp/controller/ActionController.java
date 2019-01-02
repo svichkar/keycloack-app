00427b607aba449253fc20092dc138e1f3763800
Konstantin Svichkar <ksvichkar@connecture.com>
2019-01-03 00:47:26 +0200

    login & logout initial commit

--- src/main/java/com/svichkar/keycloackapp/controller/ActionController.java
+++ src/main/java/com/svichkar/keycloackapp/controller/ActionController.java
@@ -3,15 +3,18 @@ package com.svichkar.keycloackapp.controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ActionController
 {
   @GetMapping(value = "/")
-  public String app()
+  public String app(ModelMap model)
   {
+    model.addAttribute("authenticated", SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
     return "index";
   }
 
--- src/main/resources/templates/header.ftl
+++ src/main/resources/templates/header.ftl
@@ -5,7 +5,13 @@
         </div>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="help"><span class="glyphicon glyphicon-question-sign"></span> Help</a></li>
-            <li><a href=${rc}><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li>
+                <#if authenticated>
+                    <a href="/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                <#else>
+                    <a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                </#if>
+            </li>
         </ul>
     </div>
 </nav>
